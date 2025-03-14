2ff069716ba07b1328c2be55b5f91fe072c83d4a
密度跃层 <pycnocline@hotmail.com>
2025-03-15 01:42:55 +0800

    实现定向照明，优化项目结构

--- Just/shaders/basic_frag.glsl
+++ Just/shaders/basic_frag.glsl
@@ -2,6 +2,8 @@
 
 uniform sampler2D gtexture;     // 获取所有纹理
 uniform sampler2D lightmap;     // 光照纹理
+uniform mat4 gbufferModelViewInverse;
+uniform vec3 shadowLightPosition;   // 阴影光源位置
 
 /* DRAWBUFFERS:0 */
 layout(location = 0) out vec4 outColor0;
@@ -9,8 +11,15 @@ layout(location = 0) out vec4 outColor0;
 in vec2 texCoord;
 in vec3 foliageColor;  // 顶点颜色
 in vec2 lightMapCoords;   // 光照坐标
+in vec3 geoNormal;    // 几何法线
 
 void main() {
+    vec3 shadowLightDirection = normalize(mat3(gbufferModelViewInverse) * shadowLightPosition);
+
+    vec3 worldGeoNormal = mat3(gbufferModelViewInverse) * geoNormal;    // 世界坐标系法线
+
+    float lightBrightness = clamp(dot(shadowLightDirection, worldGeoNormal), 0.2, 1.0);
+
     vec3 lightColor = pow(texture(lightmap, vec2(lightMapCoords)).rgb, vec3(2.2));  // 从光照纹理中获取颜色
 
     vec4 outputColorData = pow(texture(gtexture, texCoord), vec4(2.2));    // 从纹理中获取颜色赋值给outColor
@@ -21,5 +30,6 @@ void main() {
         discard;
     }
 
-    outColor0 = pow(vec4(outputColor, transparency), vec4(1/2.2));    // 最终输出颜色
+    outputColor *= lightBrightness;     // 乘光照亮度
+    outColor0 = vec4(pow(outputColor, vec3(1 / 2.2)), transparency);    // 最终输出颜色
 }
--- Just/shaders/basic_vert.glsl
+++ Just/shaders/basic_vert.glsl
@@ -4,17 +4,22 @@ in vec3 vaPosition;     // 顶点坐标
 in vec2 vaUV0;          // 纹理UV坐标
 in vec4 vaColor;        // 顶点颜色
 in ivec2 vaUV2;         // 块亮度图，ivec2:整型
+in vec3 vaNormal;       // 顶点法线
 
 uniform vec3 chunkOffset;       // 块偏移量
 uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
 uniform mat4 gbufferModelViewInverse;
+uniform mat3 normalMatrix;
 
 out vec2 texCoord;    // 纹理坐标 texture coordinate
 out vec3 foliageColor;  // 顶点颜色
 out vec2 lightMapCoords;   // 光照坐标
+out vec3 geoNormal;    // 几何法线
 
 void main() {
+    geoNormal = normalMatrix * vaNormal;    // 面法线赋值给geoNormal
+
     texCoord = vaUV0;   // 纹理坐标赋值给texCoord
     foliageColor = vaColor.rgb;     // 顶点颜色赋值给foliageColor
     lightMapCoords = vaUV2 * (1.0 / 256.0) + (1.0 / 32.0);     // 光照坐标赋值给lightMapCoords
