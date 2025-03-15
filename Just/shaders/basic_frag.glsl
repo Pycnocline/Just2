1fc1405d4becdaaf5181fb267174863581c57377
密度跃层 <pycnocline@hotmail.com>
2025-03-15 14:48:11 +0800

    增加对法线纹理的支持，调整代码结构

--- Just/shaders/basic_frag.glsl
+++ Just/shaders/basic_frag.glsl
@@ -1,35 +1,58 @@
 #version 460
 
+in vec2 texCoord;
+in vec3 foliageColor;  // 顶点颜色
+in vec2 lightMapCoords;   // 光照坐标
+in vec3 geoNormal;    // 几何法线
+in vec4 tangent;      // 切线
+
 uniform sampler2D gtexture;     // 获取所有纹理
 uniform sampler2D lightmap;     // 光照纹理
+uniform sampler2D normals;      // 法线纹理
+uniform sampler2D specular;     // 高光
 uniform mat4 gbufferModelViewInverse;
 uniform vec3 shadowLightPosition;   // 阴影光源位置
 
 /* DRAWBUFFERS:0 */
 layout(location = 0) out vec4 outColor0;
 
-in vec2 texCoord;
-in vec3 foliageColor;  // 顶点颜色
-in vec2 lightMapCoords;   // 光照坐标
-in vec3 geoNormal;    // 几何法线
+mat3 tbnNormalTangent(vec3 normal, vec3 tangent) {
+    vec3 bitangent = cross(tangent, normal);
+    return mat3(tangent, bitangent, normal);
+}
 
 void main() {
+    // 通过法线计算定向光照结果
     vec3 shadowLightDirection = normalize(mat3(gbufferModelViewInverse) * shadowLightPosition);
-
     vec3 worldGeoNormal = mat3(gbufferModelViewInverse) * geoNormal;    // 世界坐标系法线
+    vec3 worldTangent = mat3(gbufferModelViewInverse) * tangent.xyz;    // 世界坐标系切线
+    
 
-    float lightBrightness = clamp(dot(shadowLightDirection, worldGeoNormal), 0.2, 1.0);
+    // 处理带有法线的纹理
+    vec4 normalData = texture(normals, texCoord) * 2.0 - 1.0;
+    vec3 normalNormalSpace = vec3(normalData.xy, sqrt(1.0 - dot(normalData.xy, normalData.xy)));
+    mat3 TBN = tbnNormalTangent(worldGeoNormal, worldTangent);
+    vec3 normalWorldSpace = TBN * normalNormalSpace;
 
+    float lightBrightness = clamp(dot(shadowLightDirection, normalWorldSpace), 0.2, 1.0);
+
+    // 处理高光
+    vec4 specularData = texture(specular, texCoord);
+    float perceptualSmoothness = specularData.r;
+    float roughness = pow(1.0 - perceptualSmoothness, 2.0); // 计算粗糙度
+
+    // 根据方块的亮度等级获取光照纹理
     vec3 lightColor = pow(texture(lightmap, vec2(lightMapCoords)).rgb, vec3(2.2));  // 从光照纹理中获取颜色
-
     vec4 outputColorData = pow(texture(gtexture, texCoord), vec4(2.2));    // 从纹理中获取颜色赋值给outColor
     vec3 outputColor = outputColorData.rgb * pow(foliageColor,vec3(2.2)) * lightColor;    // 顶点颜色乘纹理颜色乘光照颜色
+    
+    outputColor *= lightBrightness;     // 乘光照亮度
 
+    // 透明度检查
     float transparency = outputColorData.a;    // 获取透明度
     if (transparency < 0.1) {      // 如果透明度小于0.1那么识别为透明
         discard;
     }
 
-    outputColor *= lightBrightness;     // 乘光照亮度
     outColor0 = vec4(pow(outputColor, vec3(1 / 2.2)), transparency);    // 最终输出颜色
 }
--- Just/shaders/basic_vert.glsl
+++ Just/shaders/basic_vert.glsl
@@ -5,6 +5,7 @@ in vec2 vaUV0;          // 纹理UV坐标
 in vec4 vaColor;        // 顶点颜色
 in ivec2 vaUV2;         // 块亮度图，ivec2:整型
 in vec3 vaNormal;       // 顶点法线
+in vec4 at_tangent;     // 切线
 
 uniform vec3 chunkOffset;       // 块偏移量
 uniform mat4 modelViewMatrix;
@@ -16,8 +17,10 @@ out vec2 texCoord;    // 纹理坐标 texture coordinate
 out vec3 foliageColor;  // 顶点颜色
 out vec2 lightMapCoords;   // 光照坐标
 out vec3 geoNormal;    // 几何法线
+out vec4 tangent;      // 切线
 
 void main() {
+    tangent = vec4(normalize(normalMatrix * at_tangent.rgb), at_tangent.a);   // 切线赋值给tangent
     geoNormal = normalMatrix * vaNormal;    // 面法线赋值给geoNormal
 
     texCoord = vaUV0;   // 纹理坐标赋值给texCoord
